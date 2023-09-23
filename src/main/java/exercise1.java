563de6ae7de630028e6e38d4fe506e17be957563
lfogawa <lfogawa0@gmail.com>
2023-09-22 22:22:13 -0300

    initial commit

---
+++ src/main/java/exercise1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class exercise1 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu nome");
+        String nome = scanner.nextLine();
+
+        System.out.println("Agora, digite seu sobrenome");
+        String sobrenome = scanner.nextLine();
+
+        System.out.printf("Seja bem-vindo(a), %s %s", nome, sobrenome);
+    }
+}
