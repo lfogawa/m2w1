ed93455fd81ad5eaf82fae57ffc4a1c21d5b0190
lfogawa <lfogawa0@gmail.com>
2023-09-23 18:50:15 -0300

    exercise 3 finished

---
+++ src/main/java/exercise3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class exercise3 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número inteiro");
+        int numeroInteiro = scanner.nextInt();
+
+        if (numeroInteiro % 2 == 0) {
+            System.out.print("O número é par");
+        } else {
+            System.out.print("O número é ímpar");
+        }
+
+        scanner.close();
+    }
+}
